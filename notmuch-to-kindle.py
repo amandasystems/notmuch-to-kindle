ab0196243c2fac692e9a31345cf07611ef132f06
Albin Stjerna <albin@eval.nu>
2011-05-14 18:18:46 +0200

    Fix some bugs.

--- notmuch-to-kindle.py
+++ notmuch-to-kindle.py
@@ -5,7 +5,7 @@
 # way you like, don't complain if it breaks. Or do, and suggest
 # improvements.
 
-import ConfigParser, subprocess, notmuch, os, tempfile, base64, datetime, re, shutil
+import ConfigParser, subprocess, notmuch, os, tempfile, base64, datetime, re, shutil, mimetypes, string
 from email.parser import Parser
 
 def find_url_in_mail(mail):
@@ -27,7 +27,7 @@ mails = nm_query.search_messages()
 maillist = []
 
 max_entries = config.getint("main", "max")
-
+types_to_convert = ['text/html']
 iterator = 0
 for m in mails:
     if iterator < max_entries:
@@ -36,59 +36,85 @@ for m in mails:
     else:
         break
 
+def sanitize_filename(fn):
+    valid_chars = frozenset("-_.() %s%s" % (string.ascii_letters, string.digits))
+    return ''.join(c for c in fn if c in valid_chars)
 
-def get_html_multipart(mail):
-    "Return the text/html part of a multipart mail, or simply the first part if not multipart"
-    if not mail.is_multipart():
-        return mail.get_payload()
-    else:
-        payload = mail.get_payload()
-        for load in payload:
-            if load.get_content_type() == "text/html":
-                return load.get_payload()
-            elif load.is_multipart():
-                return get_html_multipart(load)
-        # if we got here we're screwed
+
+# def get_html_multipart(mail):
+#     "Return the text/html part of a multipart mail, or simply the first part if not multipart"
+            
+#     if not mail.is_multipart():
+#         return mail.get_payload(decode=True)
+#     else:
+#         for part in mail.walk():
+# #            if part.get_content_maintype() == 'application':
+#  #               print part.get_content_type()
+
+#             # iterate over entire mail. if there's a text/html and a text/plain, prefer html.
+#             # if there's an application/pdf, extract it.
+#             # ext = mimetypes.guess_extension(part.get_content_type()) (see http://docs.python.org/library/email-examples.html)
+
+#             # don't bother with multipart, they're just containers
+#             if part.get_content_maintype() == 'multipart':
+#                 continue
+#             else:
+#                 print part.get_content_type()
+
+#         payloads = mail.get_payload()
+#         for load in payloads:
+#             if load.get_content_type() == "text/html":
+#                 return load.get_payload(decode=True)
+#             elif load.is_multipart():
+#                 return get_html_multipart(load)
+#         # if we got here we're screwed
+
 
 tempfolder = tempfile.mkdtemp()
 
 def gen_item (mail):
-    fp = open(mail.get_filename(), 'r')
-    mailfile = Parser().parse(fp)
-    url = find_url_in_mail(mailfile)
-    
-    content = get_html_multipart(mailfile)
-    if content == None:
-        if not mailfile.get_payload()[0] == None:            
-            content = mailfile.get_payload()[0].as_string()
-        else:
-            print "Warning! No content in email %s!" % mail.get_message_id()
-            return
+    with open(mail.get_filename(), 'r') as fp:
+        mailfile = Parser().parse(fp)
+    url = find_url_in_mail(mailfile)    
+    counter = 1
 
-    fp.close()
-
-    base_filename="%s/%s" % (tempfolder, base64.urlsafe_b64encode(mail.get_message_id()))
-    f = open(base_filename + ".html", 'w')
-    f.write(content)
-    f.close()
-
-    p = subprocess.Popen(
-        ['ebook-convert',
-         base_filename + ".html", 
-         base_filename + ".mobi",
-         '--input-profile=%s' % config.get("ebook-convert", "input_profile"),
-         '--authors="%s"'  % mail.get_header("From"),
-         '--pubdate="%d"'  % mail.get_date(),
-         '--title="%s"'    % mail.get_header("Subject"),
-         '--comments="%s"' % url], 
-        stdout=subprocess.PIPE, stderr = subprocess.PIPE)
-    stdout, stderr = p.communicate()
-    return
+    for part in mailfile.walk():
+        # multipart/* are just containers
+        if part.get_content_maintype() == 'multipart':
+            continue
+        # Applications should really sanitize the given filename so that an
+        # email message can't be used to overwrite important files
+        filename = part.get_filename()
+        if not filename:
+            ext = mimetypes.guess_extension(part.get_content_type())
+            if not ext:
+                # Use a generic bag-of-bits extension
+                ext = '.bin'
+            filename = '%s-part-%03d%s' % (mail.get_header("Subject"), counter, ext)
+        filename = sanitize_filename(filename)
+        print filename
+        counter += 1
+        with open(os.path.join(tempfolder, filename), 'wb') as fp:
+            fp.write(part.get_payload(decode=True))
+            fp.close()
+        if part.get_content_type() in types_to_convert:
+            # html files needs conversion.
+            p = subprocess.Popen(
+                ['ebook-convert',
+                 os.path.join(tempfolder, filename), 
+                 os.path.join(tempfolder, os.path.splitext(filename)[0] + ".mobi"),
+                 '--input-profile=%s' % config.get("ebook-convert", "input_profile"),
+                 '--authors="%s"'  % mail.get_header("From"),
+                 '--pubdate="%d"'  % mail.get_date(),
+                 '--title="%s"'    % mail.get_header("Subject"),
+                 '--comments="%s"' % url], 
+                stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+            stdout, stderr = p.communicate()
 
 
 map(gen_item, maillist)
 
-for f in filter(lambda x: x.endswith(".mobi"), os.listdir(tempfolder)):
+for f in filter(lambda x: (x.endswith(".mobi") or x.endswith(".pdf")), os.listdir(tempfolder)):
     path = "%s/%s" % (tempfolder, f)
     print "now copying " + path + " to " + config.get("main", "target")
     shutil.copy(path, config.get("main", "target"))
