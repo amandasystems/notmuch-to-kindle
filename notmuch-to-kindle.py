e32669083f2c3a54d2572c3fa5a3d457a045b598
Albin Stjerna <albin@eval.nu>
2011-05-22 00:31:30 +0200

    More silent operation, use abiword to convert .doc attachments (that are recognized as such) to html, and feed that to calibre. Won't use proper metadata, though.

--- notmuch-to-kindle.py
+++ notmuch-to-kindle.py
@@ -5,7 +5,7 @@
 # way you like, don't complain if it breaks. Or do, and suggest
 # improvements.
 
-import ConfigParser, subprocess, notmuch, os, tempfile, datetime, shutil, mimetypes, string
+import ConfigParser, subprocess, notmuch, os, tempfile, datetime, shutil, mimetypes, string, sys
 from email.parser import Parser
 
 def find_url_in_mail(mail):
@@ -27,7 +27,7 @@ mails = nm_query.search_messages()
 maillist = []
 
 max_entries = config.getint("main", "max")
-types_to_convert = ['text/html'] # TODO add .epub etc.
+types_to_convert = ['text/html', 'application/epub+zip']
 iterator = 0
 for m in mails:
     if iterator < max_entries:
@@ -39,6 +39,20 @@ for m in mails:
 def sanitize_filename(fn):
     valid_chars = frozenset("-_.() %s%s" % (string.ascii_letters, string.digits))
     return ''.join(c for c in fn if c in valid_chars)
+
+def calibre_to_mobi(path, filename, authors, date, title, url):
+    p = subprocess.Popen(
+        ['ebook-convert',
+         os.path.join(path, filename), 
+         os.path.join(path, os.path.splitext(filename)[0] + ".mobi"),
+         '--input-profile=%s' % config.get("ebook-convert", "input_profile"),
+         '--authors="%s"'  % authors, 
+         '--pubdate="%d"'  % date, 
+         '--title="%s"'    % title, 
+         '--comments="%s"' % url], 
+        stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    
 
 tempfolder = tempfile.mkdtemp()
 
@@ -63,34 +77,41 @@ def gen_item (mail):
                 ext = '.bin'
             filename = '%s-part-%03d%s' % (mail.get_header("Subject"), counter, ext)
         filename = sanitize_filename(filename)
-        print filename
         counter += 1
         with open(os.path.join(tempfolder, filename), 'wb') as fp:
-            fp.write(part.get_payload(decode=True))
-            fp.close()
+            payload = part.get_payload(decode=True)
+            if not payload == None:
+                fp.write(part.get_payload(decode=True))
+                fp.close()
+            else:
+                print >> sys.stderr, "Error: Unable to extract payload from filename %s, part dump following:" % filename
+                print >> sys.stderr, part
+                print >> sys.stderr, "End of part dump for file named %s." % filename
+        basename, ext = os.path.splitext(filename)
+        if ext == '.doc':
+            # process with abiword!
+            try:
+                p = subprocess.Popen(['abiword',
+                                      '--to=%s.html' % os.path.join(tempfolder, basename),
+                                      os.path.join(tempfolder, filename)],
+                                     stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+                stdout, stderr = p.communicate()
+            except OSError, e:
+                print >>sys.stderr, "Failed to convert to html using abiword. Possibly missing abiword? Got error: ", e
+            # TODO implement optional arguments in calibre_to_mobi
+            calibre_to_mobi(tempfolder, basename + '.html', "", 0, "", "")
         if part.get_content_type() in types_to_convert:
             # html files needs conversion.
-            # TODO if part is .doc -- preprocess with antiword
-            p = subprocess.Popen(
-                ['ebook-convert',
-                 os.path.join(tempfolder, filename), 
-                 os.path.join(tempfolder, os.path.splitext(filename)[0] + ".mobi"),
-                 '--input-profile=%s' % config.get("ebook-convert", "input_profile"),
-                 '--authors="%s"'  % mail.get_header("From"),
-                 '--pubdate="%d"'  % mail.get_date(),
-                 '--title="%s"'    % mail.get_header("Subject"),
-                 '--comments="%s"' % url], 
-                stdout=subprocess.PIPE, stderr = subprocess.PIPE)
-            stdout, stderr = p.communicate()
-
+            calibre_to_mobi(tempfolder, filename, mail.get_header("From"), 
+                            mail.get_date(), mail.get_header("Subject"), url)
 
 map(gen_item, maillist)
 
 try:
     for f in filter(lambda x: (os.path.splitext(x)[1] in [".pdf", ".mobi", ".azw"]), os.listdir(tempfolder)):
         path = "%s/%s" % (tempfolder, f)
-        print "now copying " + path + " to " + config.get("main", "target")
+#        print "now copying " + path + " to " + config.get("main", "target")
         shutil.copy(path, config.get("main", "target"))
 finally:
-    print "removed temporary files"
+ #   print "removed temporary files"
     shutil.rmtree(tempfolder)
